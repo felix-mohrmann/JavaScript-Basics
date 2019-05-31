c21d6d08d905322f9c5097f985d5a6bd08293bff
Felix Mohrmann <Felix mohrmann.felix@gmail.comgit config --global user.email Felix>
2019-06-01 04:10:34 +1000

    reviewed basic concepts of JS

---
+++ main.js
@@ -0,0 +1,23 @@
+// console.log('Hello World');
+// console.error('This is an error');
+// console.warn('This is an warning');
+
+// let -> reasignable
+// const -> constant
+
+// primitive data types: String, Numbers, Boolean, null, undefined
+const name = 'John';
+const age = 30;
+const rating = 4.5;
+const isCool = true;
+const x = null;
+const y = undefined;
+let z; //-> also undefined
+
+console.log(typeof z);
+
+// Concatenation
+console.log('My name is ' + name + ' and I am ' + age);
+
+
+
